bdd944559297c097e7d6dc7d08845be1a32d9290
isaevsergey28 <sergey6929565i@mail.ru>
2021-09-13 00:07:43 +0300

    Trail after player added

---
+++ Assets/Scripts/PlayerTrail.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTrail : MonoBehaviour
+{
+    private void Update()
+    {
+        transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
+    }
+}
